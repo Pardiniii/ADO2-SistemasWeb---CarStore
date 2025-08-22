f9e7bf95abff78f9455bfaa9bf3336e2611cb7db
gustavo.gpardini <ggpardini@gmail.com>
2025-08-21 21:51:09 -0300

    First commit

---
+++ src/main/java/br/com/carstore/controller/IndexController.java
@@ -0,0 +1,26 @@
+package br.com.carstore.controller;
+
+import br.com.carstore.model.Car;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "<h1>Hello world</h1>";
+    }
+
+    @GetMapping("/car")
+    public ResponseEntity<Car> home() {
+        Car car = new Car();
+        car.setName("jetta");
+        car.setColor("preto");
+
+        return ResponseEntity.ok(car);
+    }
+
+}
